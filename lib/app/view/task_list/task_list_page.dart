f24d710e04bce8403f8d4b8e5e11929d8eccd688
GPplay <gybramdali2002@hotmail.com>
2024-04-15 10:21:56 -0500

    refactorice el codigo

--- lib/app/view/task_list/task_list_page.dart
+++ lib/app/view/task_list/task_list_page.dart
@@ -8,42 +8,11 @@ const TaskListPage({Key? key}) : super(key: key);
 @override
 Widget build(BuildContext context) {
   return Scaffold(
-  body: Column(
+  body: const Column(
     crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-          Container(
-          width: double.infinity,
-          color: Theme.of(context).colorScheme.primary,
-            child: Column(
-              children: [
-          const Row(children: [Decoracion()]),
-    Column(
-          children: [
-                      Image.asset(
-                      'assets/image/tasks-list-image.png',
-                      width: 120,
-                      height: 120,
-                      ),
-                      const SizedBox(height: 16),
-                      const H1('Completa tus tareas', color: Colors.white),
-                  ],
-                )
-              ],
-            ),
-        ),
-        const Padding(
-          padding: EdgeInsets.only(left: 30, right: 30, top: 35),
-          child: H1('Tareas')
-          ),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30),
-            child: ListView.separated(
-            itemBuilder: (_, index) => _TaskItem(taskList[index]), 
-            separatorBuilder: (_,__,) => const SizedBox(height: 16),
-            itemCount: taskList.length,),
-          )
-        ),
+          _Header(),
+          Expanded(child: _TaskList()),
       ],
 ),
 floatingActionButton: FloatingActionButton(
@@ -81,9 +50,65 @@ class _TaskItem extends StatelessWidget {
   }
 }
 
-final taskList = <Task> [
-  Task('hacer la tarea'),
-  Task('ver el partido'),
-  Task('sacar a el perro'),
-  Task('breve'),
-];
+class _Header extends StatelessWidget {
+  const _Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+          width: double.infinity,
+          color: Theme.of(context).colorScheme.primary,
+            child: Column(
+              children: [
+          const Row(children: [Decoracion()]),
+    Column(
+          children: [
+                      Image.asset(
+                      'assets/image/tasks-list-image.png',
+                      width: 120,
+                      height: 120,
+                      ),
+                      const SizedBox(height: 16),
+                      const H1('Completa tus tareas', color: Colors.white),
+                  ],
+                )
+              ],
+            ),
+        );
+  }
+}
+
+class _TaskList extends StatefulWidget {
+  const _TaskList({super.key});
+  @override
+  State<_TaskList> createState() => _TaskListState();
+}
+
+class _TaskListState extends State<_TaskList> {
+
+  final taskList = <Task> [
+    Task('hacer la tarea'),
+    Task('ver el partido'),
+    Task('sacar a el perro'),
+    Task('breve'),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 25),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const H1('Tareas'),
+          Expanded(
+            child: ListView.separated(
+              itemBuilder: (_, index) => _TaskItem(taskList[index]), 
+              separatorBuilder: (_,__,) => const SizedBox(height: 16),
+              itemCount: taskList.length,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
